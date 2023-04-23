1a1c8330d9013268aa40fb60cf939913b1186e43
Maximilian Biebl <maximilian.biebl@mni.thm.de>
2023-04-23 20:59:33 +0200

    character is now facing in his walking direction

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,19 @@ using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private const float up = 1;
+    private const float down = -1;
+    private const float left = -1;
+    private const float right = 1;
+
+    //TODO: add different movement types
+    enum Directions
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
 
     public float moveSpeed = 1f;
     public Rigidbody2D rb;
@@ -14,7 +27,9 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movementInput;
     private Vector2 _smoothMovementInput;
     private Vector2 _movementInputSmoothVelocity;
-    
+
+    private Directions currentDircetion = Directions.Right;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -31,21 +46,73 @@ public class PlayerMovement : MonoBehaviour
             0.1f);
         rb.velocity = _smoothMovementInput * moveSpeed;
     }
-    
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // Input
-        //movement.x = Input.GetAxisRaw("Horizontal");
-        //movement.y = Input.GetAxisRaw("Vertical");
-        
-        animator.SetFloat("Horizontal", _smoothMovementInput.x);
-        animator.SetFloat("Vertical", _smoothMovementInput.y);
+        getDirection();
+        faceDirection();
         animator.SetFloat("Speed", _smoothMovementInput.sqrMagnitude);
-        print("hallo");
-       
     }
-    
+
+    //note the direction by the current user input
+    void getDirection()
+    {
+        if (_movementInput.x == left)
+        {
+            currentDircetion = Directions.Left;
+        }
+        else if (_movementInput.x == right)
+        {
+            currentDircetion = Directions.Right;
+        }
+        else if (_movementInput.y == up)
+        {
+            currentDircetion = Directions.Up;
+        }
+        else if (_movementInput.y == down)
+        {
+            currentDircetion = Directions.Down;
+        }
+    }
+
+    //let the character in the walking direction
+    void faceDirection()
+    {
+        Vector3 scale = transform.localScale;
+        Quaternion rotation = transform.rotation;
+        //TODO: reduce magic numbers
+        switch (currentDircetion)
+        {
+                case Directions.Right:
+                {
+                    scale.x = 1;
+                    rotation.z = 0f;
+                    break;
+                }
+                case Directions.Left:
+                {
+                    scale.x = -1;
+                    rotation.z = 0f;
+                    break;
+                }
+                case Directions.Up:
+                {
+                    scale.x = 1;
+                    rotation.z = 0.7f;
+                    break;
+                }
+                case Directions.Down:
+                {
+                    scale.x = 1;
+                    rotation.z = -0.7f;
+                    break;
+                }
+        }
+        transform.localScale = scale;
+        transform.rotation = rotation;
+    }
+
     //default Method for movement keys
     private void OnMove(InputValue inputValue)
     {
