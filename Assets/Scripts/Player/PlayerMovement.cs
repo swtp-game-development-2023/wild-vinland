a757b7030e5a6b08a202501848e6a285bf8940d0
Jannis Magdeburg <82678309+19jama94@users.noreply.github.com>
2023-04-24 19:22:30 +0200

    added Crouch & Run Function to PlayerMovement with Animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,8 +11,14 @@ public class PlayerMovement : MonoBehaviour
     private const float down = -1;
     private const float left = -1;
     private const float right = 1;
-
-    //TODO: add different movement types
+    
+    enum MovementTypes
+    {
+        Idle,
+        Walk,
+        Crouch,
+        Run
+    }
     enum Directions
     {
         Up,
@@ -21,18 +27,27 @@ public class PlayerMovement : MonoBehaviour
         Right
     }
     
-    public float moveSpeed = 1f;
+    [SerializeField]
+    private float moveSpeed = 1f;
+    private const float DefaultMovementSpeed = 5f;
+    
     public Rigidbody2D rb;
     public Animator animator;
     private Vector2 _movementInput;
     private Vector2 _smoothMovementInput;
     private Vector2 _movementInputSmoothVelocity;
-
-    private Directions currentDircetion = Directions.Right;
+    private Directions _currentDirection = Directions.Right;
+    private MovementTypes _currentMovementType = MovementTypes.Idle; 
+    
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int IsMoving = Animator.StringToHash("isMoving");
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+    private static readonly int IsCrouching = Animator.StringToHash("isCrouching");
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     //change the players position based on input
@@ -50,9 +65,54 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        getDirection();
-        faceDirection();
-        animator.SetFloat("Speed", _smoothMovementInput.sqrMagnitude);
+        if (_movementInput != Vector2.zero)
+        {
+            getDirection();
+            faceDirection();
+            animator.SetFloat(Speed, _smoothMovementInput.sqrMagnitude);
+            
+            animator.SetBool(IsMoving, true);
+
+        }
+        else
+        {
+            animator.SetBool(IsMoving, false);
+        }
+        
+    }
+    
+    //default Method for movement keys
+    private void OnMove(InputValue inputValue)
+    {
+        _movementInput = inputValue.Get<Vector2>();
+        _currentMovementType = MovementTypes.Walk;
+    }
+
+    private void OnRun_Start()
+    {
+        animator.SetBool(IsRunning, true);
+        moveSpeed = 10f;
+        _currentMovementType = MovementTypes.Run;
+    }
+    
+    private void OnRun_Stop()
+    {
+        animator.SetBool(IsRunning, false);
+        moveSpeed = DefaultMovementSpeed;
+        _currentMovementType = MovementTypes.Walk;
+    }
+    
+    private void OnCrouch_Start()
+    {
+        animator.SetBool(IsCrouching, true);
+        moveSpeed = 2.5f;
+        _currentMovementType = MovementTypes.Crouch;
+    }
+    private void OnCrouch_Stop()
+    {
+        animator.SetBool(IsCrouching, false);
+        moveSpeed = DefaultMovementSpeed;
+        _currentMovementType = _movementInput != Vector2.zero ? MovementTypes.Walk : MovementTypes.Idle;
     }
 
     //note the direction by the current user input
@@ -60,19 +120,19 @@ public class PlayerMovement : MonoBehaviour
     {
         if (_movementInput.x == left)
         {
-            currentDircetion = Directions.Left;
+            _currentDirection = Directions.Left;
         }
         else if (_movementInput.x == right)
         {
-            currentDircetion = Directions.Right;
+            _currentDirection = Directions.Right;
         }
         else if (_movementInput.y == up)
         {
-            currentDircetion = Directions.Up;
+            _currentDirection = Directions.Up;
         }
         else if (_movementInput.y == down)
         {
-            currentDircetion = Directions.Down;
+            _currentDirection = Directions.Down;
         }
     }
 
@@ -82,7 +142,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 scale = transform.localScale;
         Quaternion rotation = transform.rotation;
         //TODO: reduce magic numbers
-        switch (currentDircetion)
+        switch (_currentDirection)
         {
                 case Directions.Right:
                 {
@@ -99,23 +159,19 @@ public class PlayerMovement : MonoBehaviour
                 case Directions.Up:
                 {
                     scale.x = 1;
-                    rotation.z = 0.7f;
+                    // to rotate the player 90° if pressed Up, set rotation.z to 0.7f 
+                    rotation.z = 0f;
                     break;
                 }
                 case Directions.Down:
                 {
                     scale.x = 1;
-                    rotation.z = -0.7f;
+                    // to rotate the player 90° if pressed Down, set rotation.z to -0.7f
+                    rotation.z = 0f;
                     break;
                 }
         }
         transform.localScale = scale;
         transform.rotation = rotation;
-    }
-
-    //default Method for movement keys
-    private void OnMove(InputValue inputValue)
-    {
-        _movementInput = inputValue.Get<Vector2>();
     }
 }
