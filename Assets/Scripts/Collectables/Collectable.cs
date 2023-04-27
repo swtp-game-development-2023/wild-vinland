7988c5a35c1ed19361cef0c08727a2e5bb34dc3b
Maximilian Biebl <maximilian.biebl@mni.thm.de>
2023-04-27 21:32:10 +0200

    first primitive inventory

---
+++ Assets/Scripts/Collectables/Collectable.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Instrumentation;
+using UnityEngine;
+
+
+//This interface Collectable  guarantees that a player can collect an object.
+
+public abstract class Collectable
+{
+    readonly int _id;
+    public int maxAmount = 1;
+    private int _amount = 0;
+
+    public int Amount
+    {
+        get => _amount;
+        set
+        {
+            if (_amount <= maxAmount)
+            {
+                _amount = value;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(_amount), value,
+                    "Value has to be between 0 and" + maxAmount);
+            }
+        }
+    }
+
+    protected Collectable(int id)
+    {
+        _id = id;
+    }
+
+    public override string ToString()
+    {
+        return _id.ToString();
+    }
+}
