f560facce2ddf2345d083f7764b8ed09ace4dba7
Maximilian Biebl <maximilian.biebl@mni.thm.de>
2023-06-01 13:56:50 +0200

    main menu sound is now by default off and can activated via toggle

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    public void ExitGame()
+    private bool isSoundOn = false;
+    
+    public void Start()
+    {
+        //TODO remove in final version
+        AudioListener.pause = !isSoundOn;
+    }
+
+    public void ClickedBtnNewGame()
+    {
+        //TODO
+    }
+    public void ClickedBtnLoadGame()
+    {
+        //TODO
+    }
+    
+    public void ClickedBtnCredits()
+    {
+        //TODO
+    }
+    public void ClickedBtnExit()
     {
         Debug.Log("Game Exit");
         Application.Quit();
+    }
+    
+    public void ToggleSound()
+    {
+        Debug.Log("pre " + isSoundOn);
+
+        isSoundOn = !isSoundOn;
+        Debug.Log("toggel " + isSoundOn);
+        AudioListener.pause = !isSoundOn; 
     }
 }
