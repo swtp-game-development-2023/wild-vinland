59a8c5a9eb1feb35c21c141a968a23048fd4828a
Maximilian Biebl <maximilian.biebl@mni.thm.de>
2023-06-18 13:33:50 +0200

    building can now follow the mouse

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -1,40 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
 
-public class Building : MonoBehaviour
+namespace Buildings
 {
-    [SerializeField] private GameObject prefab;
-
-    private InputManager input;
-    private bool wasBuild;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Building : MonoBehaviour
     {
-        input = new InputManager();
-        input.Player.Fire.Enable();
-    }
+        [SerializeField] private GameObject prefab;
+        private InputManager input;
+        private GameObject objBuilding;
+        private bool canDisable;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Mouse.current.leftButton.wasPressedThisFrame && !wasBuild)
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        private void OnEnable()
+        {
+            objBuilding = Instantiate(prefab, CalcGridPos(), Quaternion.identity);
+
+        }
+
+        void LateUpdate()
+        {
+            objBuilding.transform.position = CalcGridPos();
+
+            if (Mouse.current.leftButton.wasReleasedThisFrame)
+            {
+                enabled = false;
+                canDisable = false;
+            }
+        }
+        private static Vector3 CalcGridPos()
         {
             Grid grid = FindObjectOfType<Grid>();
             Vector3 mousePos = Mouse.current.position.ReadValue();
             mousePos.z = Camera.main.transform.position.z * -1;
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-            Debug.Log(mousePos +"\t" + worldPos);
-            // Runde die berechneten Weltkoordinaten auf die nächstgelegene Zelle
-            var x = Mathf.Round((worldPos.x - grid.cellSize.x / 2f) / grid.cellSize.x) * grid.cellSize.x + grid.cellSize.x / 2f;
-            var y = Mathf.Round((worldPos.y - grid.cellSize.y / 2f) / grid.cellSize.y) * grid.cellSize.y + grid.cellSize.y / 2f;
-            var outV = new Vector3(x  , y, 0);
-            Debug.Log(mousePos +"\t" + worldPos +"\t" + outV);
-            var obj =Instantiate(prefab, outV, Quaternion.identity);
-            //obj.transform.GetComponent<Building>().wasBuild = true;
-            wasBuild = true;
+            var x = Mathf.Round((worldPos.x - grid.cellSize.x / 2f) / grid.cellSize.x) * grid.cellSize.x +
+                    grid.cellSize.x / 2f;
+            var y = Mathf.Round((worldPos.y - grid.cellSize.y / 2f) / grid.cellSize.y) * grid.cellSize.y +
+                    grid.cellSize.y / 2f;
+            return new Vector3(x, y, 0);
         }
     }
 }
