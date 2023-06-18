402336e1744a759ad7c6cef43169d9649de62ac3
Maximilian Biebl <maximilian.biebl@mni.thm.de>
2023-06-18 16:57:38 +0200

    Buildings can now only be placed in certain locations.

---
+++ Assets/Scripts/Buildings/BuildScript.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
+using UnityEngine.Tilemaps;
+
+namespace Buildings
+{
+    public abstract class BuildScript : MonoBehaviour
+    {
+        [SerializeField] protected GameObject prefab;
+        private GameObject objBuilding;
+        private List<SpriteRenderer> buildingSprites;
+        protected Grid Grid;
+        private Camera mainCamera;
+        private Tilemap decoMap;
+
+
+        protected void OnEnable()
+        {
+            Grid = FindObjectOfType<Grid>();
+            mainCamera = Camera.main;
+            objBuilding = Instantiate(prefab, CalcGridPos(), Quaternion.identity);
+            buildingSprites = objBuilding.transform.GetComponentsInChildren<SpriteRenderer>().ToList();
+            decoMap = Grid.transform.Find("Deco").gameObject.GetComponent<Tilemap>();
+        }
+        
+        
+        void Update()
+        {
+            objBuilding.transform.position = CalcGridPos();
+            var canBeBuild = ProfBuildSpot(objBuilding.transform.position);
+            if (canBeBuild)
+            {
+                buildingSprites.ForEach(s => s.color = new Color(0, 255, 0)); 
+            }
+            else
+            {
+                buildingSprites.ForEach(s => s.color = new Color(255, 0, 0)); 
+            }
+
+            if (Mouse.current.leftButton.wasReleasedThisFrame && canBeBuild)
+            {
+                var gridPos = Grid.WorldToCell(objBuilding.transform.position);
+                decoMap.SetTile(gridPos, null);
+                buildingSprites.ForEach(s => s.color = new Color(255, 255, 255)); 
+                enabled = false;
+            }
+            else if (Mouse.current.rightButton.wasReleasedThisFrame)
+            {
+                Destroy(objBuilding);
+                enabled = false;
+            }
+        }
+
+        protected abstract bool ProfBuildSpot(Vector3 v);
+        
+
+        private Vector3 CalcGridPos()
+        {
+            Vector3 mousePos = Mouse.current.position.ReadValue();
+            mousePos.z = mainCamera.transform.position.z * -1;
+            Vector3 worldPos = mainCamera.ScreenToWorldPoint(mousePos);
+            var cellSize = Grid.cellSize;
+            var x = Mathf.Round((worldPos.x - cellSize.x / 2f) / cellSize.x) * cellSize.x +
+                    Grid.cellSize.x / 2f;
+            var y = Mathf.Round((worldPos.y - cellSize.y / 2f) / cellSize.y) * cellSize.y +
+                    Grid.cellSize.y / 2f;
+            return new Vector3(x, y, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Buildings/DockBuildScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Buildings;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DockBuildScript : BuildScript
+{
+    private Tilemap beachMap;
+    private Tilemap seaMap;
+    protected new void OnEnable()
+    {
+        base.OnEnable();
+        beachMap = Grid.transform.Find("Beach").gameObject.GetComponent<Tilemap>();
+        seaMap = Grid.transform.Find("Sea").gameObject.GetComponent<Tilemap>();
+    }
+    protected override bool ProfBuildSpot(Vector3 v)
+    {
+        var gridPos = Grid.WorldToCell(v);
+        var oneBelow = new Vector3Int(gridPos.x , gridPos.y -1, gridPos.z);
+        var towBelow = new Vector3Int(gridPos.x , gridPos.y -2, gridPos.z);
+        return beachMap.GetTile(gridPos) && seaMap.GetTile(oneBelow) && seaMap.GetTile(towBelow);
+    }
+}
