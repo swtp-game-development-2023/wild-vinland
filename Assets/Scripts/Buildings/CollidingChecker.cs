85cc5ea1dda332e122d3944c48d859eab494b441
Maximilian Biebl <maximilian.biebl@mni.thm.de>
2023-06-22 12:38:18 +0200

    repair two buildings on the same field bug

--- Assets/Scripts/Buildings/CollidingChecker.cs
+++ Assets/Scripts/Buildings/CollidingChecker.cs
@@ -6,22 +6,31 @@ using UnityEngine;
 public class CollidingChecker : MonoBehaviour
 {
     private bool isColliding;
-
+    private Stack<Collider2D> collider2Ds;
     public bool IsColliding
     {
         get => isColliding;
         set => isColliding = value;
     }
-    
+
+    private void Start()
+    {
+        collider2Ds = new Stack<Collider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isColliding = 0 < collider2Ds.Count;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        isColliding = true;
+        collider2Ds.Push(other);
     }
-
     
+
     void OnTriggerExit2D(Collider2D other)
     {
-        isColliding = false;
+        collider2Ds.Pop();
     }
 }
